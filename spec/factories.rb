0c31f7aa146cf7972586fe63b13c3fb07e679f91
Yoni Doron-Peters <yonidoronpeters@gmail.com>
2013-11-12 00:40:49 -0800

    add sign in tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name     "Michael Hartl"
-    email    "michael@example.com"
+    name     "Factory Girl"
+    email    "factorygirl@example.com"
     password "foobar"
     password_confirmation "foobar"
   end
